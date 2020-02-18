d35e9832d782336dbae3eb636c35dd8cc4e1097e
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-02-18 17:41:52 +0100

    ACCOUNT - app The registration is working

--- openfoodfact/accounts/urls.py
+++ openfoodfact/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import SignUpView
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('user_account/', views.user_account, name='user_page')
-
+    path('user_account/', views.user_account, name='user_page'),
 ]
