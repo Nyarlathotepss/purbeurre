d08ff5c7a18b9bb96b6a5faf16ada156492f4e32
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-02-20 13:29:51 +0100

    openfoodfact.wsgi modified

--- openfoodfact/openfoodfact/wsgi.py
+++ openfoodfact/openfoodfact/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openfoodfact.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openfoodfact.openfoodfact.settings')
 
 application = get_wsgi_application()
