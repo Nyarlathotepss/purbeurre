645c15e01c745e6df68d19a289b5f30ff71cc1ca
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-03-18 21:54:37 +0100

    change setting static_root

--- openfoodfact/settings.py
+++ openfoodfact/settings.py
@@ -22,11 +22,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'products_app/static'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
 
 # Quick-start development settings - unsuitable for production
