540dc6e9125e235db3912ce27cccdec9a5f9b1b4
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-03-19 14:01:05 +0100

    setting > STATIC URL changed

--- openfoodfact/settings.py
+++ openfoodfact/settings.py
@@ -149,6 +149,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = None
 INTERNAL_IPS = ['127.0.0.1']
 
