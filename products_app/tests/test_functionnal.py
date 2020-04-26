631d4c9fa6fb4a978425890352ff5a3b3c85f3d6
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-04-26 09:54:03 +0200

    test_functionnal.py : modified

--- products_app/tests/test_functionnal.py
+++ products_app/tests/test_functionnal.py
@@ -55,6 +55,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
         my_search.send_keys(Keys.RETURN)
         self.selenium.implicitly_wait(1)  # seconds
         self.selenium.find_element_by_name("button_id_2").click()
+        self.selenium.implicitly_wait(1)  # seconds
         self.selenium.find_element_by_id("login").is_displayed()
 
     def test_search_with_empty_query(self):
